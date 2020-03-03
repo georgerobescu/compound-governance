e8d5330b7c8cb6f4a398eaa5baf70fd5f731b7b4
Kaden Zipfel <kaden.zipfel@hotmail.com>
2020-03-02 16:50:47 -0800

    Updates

---
+++ precache-manifest.2e74b4d7874a0ee67f8b8935f286e5bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62a02650286fdd0acedbe367c0cf3cc8",
+    "url": "/compound-governance/index.html"
+  },
+  {
+    "revision": "2c7a2b7a5c35d5b90f0b",
+    "url": "/compound-governance/static/css/main.58ee7d71.chunk.css"
+  },
+  {
+    "revision": "7a39c853229d72f28d2d",
+    "url": "/compound-governance/static/js/2.8d5120af.chunk.js"
+  },
+  {
+    "revision": "2c7a2b7a5c35d5b90f0b",
+    "url": "/compound-governance/static/js/main.22d26137.chunk.js"
+  },
+  {
+    "revision": "6d0cb57d9f39bcde65d6",
+    "url": "/compound-governance/static/js/runtime-main.71ddf022.js"
+  },
+  {
+    "revision": "27e217279dc29d4a4e70760e4eeacda1",
+    "url": "/compound-governance/static/media/downvote.27e21727.svg"
+  },
+  {
+    "revision": "2dc5f234c22b895b940c5037c76616e7",
+    "url": "/compound-governance/static/media/upvote.2dc5f234.svg"
+  }
+]);
